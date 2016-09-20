a6b98105b401c3612ddb6e537dd4c54acc235c8b
Jo <jo.balme@gmail.com>
2016-09-19 19:59:16 -0700

    Ripping out functions I won't need, improving logging

--- fuses3.cpp
+++ fuses3.cpp
@@ -56,7 +56,7 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
 
 static int xmp_access(const char *path, int mask)
 {
-    log_msg("access\n");
+    log_msg("access %s\n", path);
 	int res;
 
 	res = access(path, mask);
@@ -68,7 +68,7 @@ static int xmp_access(const char *path, int mask)
 
 static int xmp_readlink(const char *path, char *buf, size_t size)
 {
-    log_msg("readlink\n");
+    log_msg("readlink %s\n", path);
 	int res;
 
 	res = readlink(path, buf, size - 1);
@@ -110,7 +110,7 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
 static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
 {
-    log_msg("mknod\n");
+    log_msg("mknod %s\n", path);
 	int res;
 
 	/* On Linux this could just be 'mknod(path, mode, rdev)' but this
@@ -129,128 +129,9 @@ static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
 	return 0;
 }
 
-static int xmp_mkdir(const char *path, mode_t mode)
-{
-    log_msg("mkdir\n");
-	int res;
-
-	res = mkdir(path, mode);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int xmp_unlink(const char *path)
-{
-    log_msg("unlink\n");
-	int res;
-
-	res = unlink(path);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int xmp_rmdir(const char *path)
-{
-    log_msg("rmdir\n");
-	int res;
-
-	res = rmdir(path);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int xmp_symlink(const char *from, const char *to)
-{
-    log_msg("symlink\n");
-	int res;
-
-	res = symlink(from, to);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int xmp_rename(const char *from, const char *to)
-{
-    log_msg("rename\n");
-	int res;
-
-	res = rename(from, to);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int xmp_link(const char *from, const char *to)
-{
-    log_msg("link\n");
-	int res;
-
-	res = link(from, to);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int xmp_chmod(const char *path, mode_t mode)
-{
-	int res;
-
-	res = chmod(path, mode);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int xmp_chown(const char *path, uid_t uid, gid_t gid)
-{
-	int res;
-
-	res = lchown(path, uid, gid);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-static int xmp_truncate(const char *path, off_t size)
-{
-	int res;
-
-	res = truncate(path, size);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-
-#ifdef HAVE_UTIMENSAT
-static int xmp_utimens(const char *path, const struct timespec ts[2])
-{
-	int res;
-
-	/* don't use utime/utimes since they follow symlinks */
-	res = utimensat(0, path, ts, AT_SYMLINK_NOFOLLOW);
-	if (res == -1)
-		return -errno;
-
-	return 0;
-}
-#endif
-
 static int xmp_open(const char *path, struct fuse_file_info *fi)
 {
-    log_msg("open\n");
+    log_msg("open %s\n", path);
     
 	int res;
 
@@ -265,7 +146,7 @@ static int xmp_open(const char *path, struct fuse_file_info *fi)
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 		    struct fuse_file_info *fi)
 {
-    log_msg("read\n");
+    log_msg("read %s\n", path);
 	int fd;
 	int res;
 
@@ -285,7 +166,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 static int xmp_write(const char *path, const char *buf, size_t size,
 		     off_t offset, struct fuse_file_info *fi)
 {
-    log_msg("write\n");
+    log_msg("write %s\n", path);
 	int fd;
 	int res;
 
@@ -304,7 +185,7 @@ static int xmp_write(const char *path, const char *buf, size_t size,
 
 static int xmp_statfs(const char *path, struct statvfs *stbuf)
 {
-    log_msg("statfs\n");
+    log_msg("statfs %s\n", path);
 	int res;
 
 	res = statvfs(path, stbuf);
@@ -313,88 +194,6 @@ static int xmp_statfs(const char *path, struct statvfs *stbuf)
 
 	return 0;
 }
-
-static int xmp_release(const char *path, struct fuse_file_info *fi)
-{
-	/* Just a stub.	 This method is optional and can safely be left
-	   unimplemented */
-
-	(void) path;
-	(void) fi;
-	return 0;
-}
-
-static int xmp_fsync(const char *path, int isdatasync,
-		     struct fuse_file_info *fi)
-{
-	/* Just a stub.	 This method is optional and can safely be left
-	   unimplemented */
-
-	(void) path;
-	(void) isdatasync;
-	(void) fi;
-	return 0;
-}
-
-#ifdef HAVE_POSIX_FALLOCATE
-static int xmp_fallocate(const char *path, int mode,
-			off_t offset, off_t length, struct fuse_file_info *fi)
-{
-	int fd;
-	int res;
-
-	(void) fi;
-
-	if (mode)
-		return -EOPNOTSUPP;
-
-	fd = open(path, O_WRONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = -posix_fallocate(fd, offset, length);
-
-	close(fd);
-	return res;
-}
-#endif
-
-#ifdef HAVE_SETXATTR
-/* xattr operations are optional and can safely be left unimplemented */
-static int xmp_setxattr(const char *path, const char *name, const char *value,
-			size_t size, int flags)
-{
-	int res = lsetxattr(path, name, value, size, flags);
-	if (res == -1)
-		return -errno;
-	return 0;
-}
-
-static int xmp_getxattr(const char *path, const char *name, char *value,
-			size_t size)
-{
-	int res = lgetxattr(path, name, value, size);
-	if (res == -1)
-		return -errno;
-	return res;
-}
-
-static int xmp_listxattr(const char *path, char *list, size_t size)
-{
-	int res = llistxattr(path, list, size);
-	if (res == -1)
-		return -errno;
-	return res;
-}
-
-static int xmp_removexattr(const char *path, const char *name)
-{
-	int res = lremovexattr(path, name);
-	if (res == -1)
-		return -errno;
-	return 0;
-}
-#endif /* HAVE_SETXATTR */
 
 int main(int argc, char **argv)
 {
@@ -406,34 +205,10 @@ int main(int argc, char **argv)
 	xmp_oper.readlink	= xmp_readlink;
 	xmp_oper.readdir	= xmp_readdir;
 	xmp_oper.mknod		= xmp_mknod;
-	xmp_oper.mkdir		= xmp_mkdir;
-	xmp_oper.symlink	= xmp_symlink;
-	xmp_oper.unlink		= xmp_unlink;
-	xmp_oper.rmdir		= xmp_rmdir;
-	xmp_oper.rename		= xmp_rename;
-	xmp_oper.link		= xmp_link;
-	xmp_oper.chmod		= xmp_chmod;
-	xmp_oper.chown		= xmp_chown;
-	xmp_oper.truncate	= xmp_truncate;
-#ifdef HAVE_UTIMENSAT
-	xmp_oper.utimens	= xmp_utimens;
-#endif
 	xmp_oper.open		= xmp_open;
 	xmp_oper.read		= xmp_read;
 	xmp_oper.write		= xmp_write;
 	xmp_oper.statfs		= xmp_statfs;
-	xmp_oper.release	= xmp_release;
-	xmp_oper.fsync		= xmp_fsync;
-#ifdef HAVE_POSIX_FALLOCATE
-	xmp_oper.fallocate	= xmp_fallocate;
-#endif
-#ifdef HAVE_SETXATTR
-	xmp_oper.setxattr	= xmp_setxattr;
-	xmp_oper.getxattr	= xmp_getxattr;
-	xmp_oper.listxattr	= xmp_listxattr;
-	xmp_oper.removexattr	= xmp_removexattr;
-#endif
-
 
     log_fp = fopen("fuse_log.txt", "w");
     if (log_fp == NULL){
